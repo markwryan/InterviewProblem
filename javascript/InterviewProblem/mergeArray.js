7ad1255a003060428e8bb26a3079d3e319741818
Mark Ryan <mryan@axs.com>
2017-12-18 12:15:42 -0500

    Add in my js solution

--- javascript/InterviewProblem/mergeArray.js
+++ javascript/InterviewProblem/mergeArray.js
@@ -1,3 +1,36 @@
 exports.merge = function(array1, array2) {
-    return [];
+
+    if(array1 == null) {
+        if(array2 == null) {
+            return null;
+        }
+        return array2;
+    }
+
+    if(array2 == null) {
+        return array1;
+    }
+
+    var totalElements = array1.length + array2.length;
+    var elementsRemaining = totalElements > 0;
+    var mergedElements = [];
+    var elementIndex = 0;
+
+    while(elementsRemaining) {
+        if(elementIndex < array1.length) {
+            mergedElements.push(array1[elementIndex]);
+        }
+
+        if(elementIndex < array2.length) {
+            mergedElements.push(array2[elementIndex]);
+        }
+
+        if(elementIndex >= array1.length && elementIndex >= array2.length) {
+            elementsRemaining = false;
+        }
+        elementIndex++;
+    }
+
+    return mergedElements;
 };
+
