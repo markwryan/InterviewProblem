192cb36f824fc2dc46783acc244007364357e7ac
Mark Ryan <mryan@axs.com>
2017-12-18 12:37:44 -0500

    Updated tests and added mark's solutions

--- c-sharp/InterviewProblem/InterviewProblem/MergeArray.cs
+++ c-sharp/InterviewProblem/InterviewProblem/MergeArray.cs
@@ -1,16 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InterviewProblem
+﻿namespace InterviewProblem
 {
     public class MergeArray
     {
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+            if (array1 == null)
+            {
+                return array2;
+            }
+            if (array2 == null)
+            {
+                return array1;
+            }
+            
+            var totalElements = array1.Length + array2.Length;
+            var elementsRemain = totalElements > 0;
+            var mergedElements = new char[totalElements];
+            var elementIndex = 0;
+            var mergedIndex = 0;
+            while (elementsRemain)
+            {
+                if (elementIndex < array1.Length)
+                {
+                    mergedElements[mergedIndex] = array1[elementIndex];
+                    mergedIndex++;
+                }
+
+                if (elementIndex < array2.Length)
+                {
+                    mergedElements[mergedIndex] = array2[elementIndex];
+                    mergedIndex++;
+                }
+
+                if (elementIndex >= array1.Length && elementIndex >= array2.Length)
+                    elementsRemain = false;
+                elementIndex++;
+            }
+            return mergedElements;
         }
     }
 }
--- c-sharp/InterviewProblem/InterviewProblem/MergeTests.cs
+++ c-sharp/InterviewProblem/InterviewProblem/MergeTests.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace InterviewProblem
 {
-    [TestClass]
+    [TestFixture]
     public class MergeTests
     {
-        [TestMethod]
+        [Test]
         public void MergeEqualItemsTest()
         {
-            var charArray1 = new[] {'a', 'b', 'c'};
-            var charArray2 = new[] {'1', '2', '3'};
+            var array1 = new[] {'a', 'b', 'c'};
+            var array2 = new[] {'1', '2', '3'};
 
             var expectedMergedArray = new[] {'a', '1', 'b', '2', 'c', '3'};
-            var actualMergedArray = MergeArray.Merge(charArray1, charArray2);
+            var actualMergedArray = MergeArray.Merge(array1, array2);
 
             for (var i = 0; i < expectedMergedArray.Length; i++)
             {
@@ -21,14 +20,14 @@ namespace InterviewProblem
             }
         }
 
-        [TestMethod]
+        [Test]
         public void MergeUnequalItemsTest()
         {
-            var charArray1 = new[] {'a', 'b'};
-            var charArray2 = new[] {'1', '2', '3', '4'};
+            var array1 = new[] {'a', 'b'};
+            var array2 = new[] {'1', '2', '3', '4'};
 
             var expectedMergedArray = new[] {'a', '1', 'b', '2', '3', '4'};
-            var actualMergedArray = MergeArray.Merge(charArray1, charArray2);
+            var actualMergedArray = MergeArray.Merge(array1, array2);
 
             for (var i = 0; i < expectedMergedArray.Length; i++)
             {
@@ -36,16 +35,16 @@ namespace InterviewProblem
             }
         }
 
-        [TestMethod]
+        [Test]
         public void MergeLargeEqualItemsTest()
         {
-            var charArray1 = new[] { 'f', '5', 'c', 'q', '9', '-', 't', '1' };
-            var charArray2 = new[] { '4', '2', '3', '/', 'e', '8', 'v', 'x' };
+            var array1 = new[] { 'f', '5', 'c', 'q', '9', '-', 't', '1' };
+            var array2 = new[] { '4', '2', '3', '/', 'e', '8', 'v', 'x' };
 
             var expectedMergedArray = new[]
                 {'f', '4', '5', '2', 'c', '3', 'q', '/', '9', 'e', '-', '8', 't', 'v', '1', 'x'};
 
-            var actualMergedArray = MergeArray.Merge(charArray1, charArray2);
+            var actualMergedArray = MergeArray.Merge(array1, array2);
 
             for (var i = 0; i < expectedMergedArray.Length; i++)
             {
@@ -53,21 +52,55 @@ namespace InterviewProblem
             }
         }
 
-        [TestMethod]
+        [Test]
         public void MergeLargeUnequalItemsTest()
         {
-            var charArray1 = new[] { 'f', '5', 'c', 'q', '9', '-', 't', '1' };
-            var charArray2 = new[] { '4', '2', '3' };
+            var array1 = new[] { 'f', '5', 'c', 'q', '9', '-', 't', '1' };
+            var array2 = new[] { '4', '2', '3' };
 
             var expectedMergedArray = new[]
                 {'f', '4', '5', '2', 'c', '3', 'q', '9', '-', 't', '1'};
 
-            var actualMergedArray = MergeArray.Merge(charArray1, charArray2);
+            var actualMergedArray = MergeArray.Merge(array1, array2);
 
             for (var i = 0; i < expectedMergedArray.Length; i++)
             {
                 Assert.AreEqual(expectedMergedArray[i], actualMergedArray[i]);
             }
+        }
+
+        [Test]
+        public void MergeWithOneNull()
+        {
+            char[] array1 = null;
+            var array2 = new[] {'4', '3', '2'};
+            var expectedMergedArray = new[] {'4', '3', '2'};
+            var actualMergedArray = MergeArray.Merge(array1, array2);
+
+            for (var i = 0; i < expectedMergedArray.Length; i++)
+            {
+                Assert.AreEqual(expectedMergedArray[i], actualMergedArray[i]);
+            }
+        }
+
+        [Test]
+        public void MergeWithBothNull()
+        {
+            char[] array1 = null;
+            char[] array2 = null;
+            var actualMergedArray = MergeArray.Merge(array1, array2);
+            Assert.IsNull(actualMergedArray);
+        }
+
+        [Test]
+        public void MergeWithBothEmpty()
+        {
+            var array1 = new char[] { };
+            var array2 = new char[] { };
+
+            var actualMergedArray = MergeArray.Merge(array1, array2);
+            
+            Assert.IsTrue(actualMergedArray.Length == 0);
         }
     }
 }
