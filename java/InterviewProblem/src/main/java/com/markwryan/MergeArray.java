3572b44f08fcc986d066aaf9539f25c85b5b4a2c
Mark Ryan <mryan@axs.com>
2017-12-18 12:48:08 -0500

    Update with Java solution.

--- java/InterviewProblem/src/main/java/com/markwryan/MergeArray.java
+++ java/InterviewProblem/src/main/java/com/markwryan/MergeArray.java
@@ -3,6 +3,33 @@ package com.markwryan;
 public class MergeArray {
 
     public static char[] Merge(char[] array1, char[] array2) {
-        return null;
+        if(array1 == null) {
+            return array2;
+        }
+        if(array2 == null) {
+            return array1;
+        }
+
+        int totalElements = array1.length + array2.length;
+        boolean elementsRemaining = totalElements > 0;
+        char[] mergedElements = new char[totalElements];
+        int elementIndex = 0;
+        int mergedIndex = 0;
+
+        while(elementsRemaining) {
+            if(elementIndex <  array1.length) {
+                mergedElements[mergedIndex] = array1[elementIndex];
+                mergedIndex++;
+            }
+
+            if(elementIndex < array2.length) {
+                mergedElements[mergedIndex] = array2[elementIndex];
+                mergedIndex++;
+            }
+
+            elementsRemaining = (elementIndex < array1.length || elementIndex < array2.length);
+            elementIndex++;
+        }
+        return mergedElements;
     }
 }
