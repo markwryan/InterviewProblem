7b22ca5967d3cffb0249ba077e49d32678e79129
Mark Ryan <mryan@axs.com>
2018-12-04 15:22:25 -0500

    Code clean up of Interview problem

--- csharp/InterviewProblem/InterviewProblem/MergeArray.cs
+++ csharp/InterviewProblem/InterviewProblem/MergeArray.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterviewProblem
 {
-    public class MergeArray
+    public static class MergeArray
     {
+	    /*
+	     * Given two arrays of variable sizes, return a new array taking one element from the head of each array in turn
+	     * until each element has been selected once and added to the new array.
+	     *
+	     * ['1','2','3','4'] and ['a','b','c'] => ['1','a','2','b','3','c','4']
+	     */
         public static char[] Merge(char[] array1, char[] array2)
         {
             throw new NotImplementedException();
