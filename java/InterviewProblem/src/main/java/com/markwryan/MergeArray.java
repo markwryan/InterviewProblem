5c3a4fdd751cd608b7733ae3adf506b8343101a3
Mark Ryan <mryan@axs.com>
2018-12-06 14:33:51 -0500

    Update java comments on method to explain goal.

--- java/InterviewProblem/src/main/java/com/markwryan/MergeArray.java
+++ java/InterviewProblem/src/main/java/com/markwryan/MergeArray.java
@@ -1,8 +1,16 @@
 package com.markwryan;
 
-public class MergeArray {
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-    public static char[] Merge(char[] array1, char[] array2) {
-        return null;
+class MergeArray {
+
+    /*
+     * Given two arrays of variable sizes, return a new array taking one element from the head of each array in turn
+     * until each element has been selected once and added to the new array.
+     *
+     * ['1','2','3','4'] and ['a','b','c'] => ['1','a','2','b','3','c','4']
+     */
+    static char[] Merge(char[] array1, char[] array2) {
+        throw new NotImplementedException();
     }
 }
