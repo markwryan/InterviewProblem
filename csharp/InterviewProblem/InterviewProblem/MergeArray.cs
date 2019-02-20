6ec4e9413b340737c8bb55dcb8225bdcf501c854
Mark Ryan <mryan@axs.com>
2019-02-20 15:34:29 -0500

    Brads final work

--- csharp/InterviewProblem/InterviewProblem/MergeArray.cs
+++ csharp/InterviewProblem/InterviewProblem/MergeArray.cs
@@ -12,7 +12,25 @@ namespace InterviewProblem
 	     */
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+	        char[] array3 = new char[8];
+	        for (int i = 0; i < array1.Length;i = i+2)
+	        {
+		        
+		        if (i > 0)
+		        {
+			        array3[i] = array1[i - 1];
+		        }
+		        else
+		        {
+			        array3[i] = array1[i];
+		        }
+	        }
+
+	        for (int j = 1; j < array2.Length; j = j + 2)
+	        {
+		        array3[j] = array2[j-1];
+	        }
+	        return array3;        
         }
     }
 }
