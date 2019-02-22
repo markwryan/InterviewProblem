8474f6296590b85cfdc76e2344b3f06c5e44485a
Mark Ryan <mryan@axs.com>
2019-02-22 14:26:02 -0500

    Alans work

--- csharp/InterviewProblem/InterviewProblem/MergeArray.cs
+++ csharp/InterviewProblem/InterviewProblem/MergeArray.cs
@@ -4,6 +4,8 @@ namespace InterviewProblem
 {
     public static class MergeArray
     {
+	    
+	    
 	    /*
 	     * Given two arrays of variable sizes, return a new array taking one element from the head of each array in turn
 	     * until each element has been selected once and added to the new array.
@@ -12,7 +14,26 @@ namespace InterviewProblem
 	     */
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+	        var a1Len = array1.Length;
+	        var a2Len = array2.Length;
+	        var totalLen = a1Len + a2Len;
+	        var array3 = new char[totalLen];
+	        
+	        //a1 = [a,b,c] 
+	        //a2 = [1,2,3]
+	        //array3 = a1[0] + a2[0] + a1[1] + a2[1] + a1[2] + a2[2]
+	        
+	        //a3[0] = 'a'
+	        //a3[1] = '1'
+	        
+	        for (var i = 0; i < totalLen; i++)
+	        {
+		        array3[i] = array1[i];
+		        
+		        array3[i + 1] = array2[i];
+	        }
+	        
+
         }
     }
 }
